2b098e3991b04bb2b1954beb2134cb46dc8abf05
lsls <ohjhooon@gmail.com>
2026-05-06 10:13:38 +0900

    feat: add sub mul func

--- utl.py
+++ utl.py
@@ -1,1 +1,8 @@
-from ~~
+from utils import add, sub, mul
+a = int(inout("insert a:"))
+b = int(input("insert b:"))
+str = input("insert str:")
+
+print("add", add(a,b))
+print("sub", sub9a,b))
+print("mul", mul(a,b))
