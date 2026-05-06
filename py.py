5059f1af1378997b3776e7d2dd9c03564bd28cd7
lsls <ohjhooon@gmail.com>
2026-05-06 09:58:13 +0900

    feat: add func

---
+++ py.py
@@ -0,0 +1,21 @@
+add(a,b)
+sub(a,b)
+mul(a,b)
+div(a,b)
+reverse(str)
+is_even(n)
+
+from ultils.py import add, sub, mul
+from main.py import div, reverse, is_even
+
+a = int(input("insert a: "))
+b = int(input("insert b: "))
+str = input("insert str:")
+
+
+print("add", add(a,b))
+print("sub", sub(a,b))
+print("mul", mul(a,b))
+print("div", div(a,b))
+print("revers", reversed_str)
+print("is_even", )
